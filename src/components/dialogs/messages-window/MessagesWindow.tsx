12e6cc1cbd4bb367c104b46649e50b0c071e3d1c
splinshady <splin.shady@gmail.com>
2022-07-27 13:11:45 +0300

    feat: add MessageWindow component

--- src/components/dialogs/messages-window/MessagesWindow.tsx
+++ src/components/dialogs/messages-window/MessagesWindow.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import styles from './MessagesWindow.module.css'
+import Message from "./message/Message";
+
+type MessagePropsType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string,
+}
 
 const MessagesWindow = () => {
     return (
         <div className={styles.messages_container + ' shadow_section'}>
-            MessagesWindow
+            <section className={styles.header}>header</section>
+            <section className={styles.body}>
+                <Message avatar={'https://cdn-icons-png.flaticon.com/512/145/145847.png'}
+                         name={'Tatuan'}
+                         message={'hi Tatuan fgsfg fgsgs f dgsdgfs gsgsfgds gs '}
+                         time={'20:33'}
+                />
+                <Message avatar={'https://cdn-icons-png.flaticon.com/512/145/145847.png'}
+                         name={'Tatuan'}
+                         message={'hi Tatuan fgsfg fgsgs f dgsdgfs gsgsfgds gs '}
+                         time={'20:33'}
+                />
+            </section>
+            <section className={styles.input_container}>
+                <textarea className={styles.input_textarea}></textarea>
+                <button>send</button>
+            </section>
         </div>
     );
 };
---
+++ src/components/dialogs/messages-window/message/Message.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from "./Message.module.css";
+
+type MessagePropsType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string,
+}
+
+const Message = (props: MessagePropsType) => {
+    return (
+        <div className={styles.message}>
+            <div className={styles.message__container}>
+                <img className={styles.message__avatar} src={props.avatar} alt={props.name}/>
+                <div className={styles.message__block}>
+                    <div className={styles.message__name_text}>
+                        <span className={styles.message__name}>{props.name}</span>
+                        <span className={styles.message__text}>{props.message}</span>
+                    </div>
+                    <span className={styles.message__time}>{props.time}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
